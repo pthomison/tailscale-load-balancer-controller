e63b4d9315e6fbcf95d5c168d1729d4dbdf25a1d
pthomison <p.thomison@gmail.com>
2023-02-28 16:29:08 -0800

    -

---
+++ golang-lb-provider/controllers/lb_pod.go
@@ -0,0 +1,128 @@
+package controllers
+
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+
+	"github.com/pthomison/errcheck"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func lbPodName(svc *corev1.Service) string {
+	return fmt.Sprintf("tailscale-lb-%s", svc.Name)
+}
+
+func lbConfigMapName(svc *corev1.Service) string {
+	return fmt.Sprintf("tailscale-lb-%s", svc.Name)
+}
+
+func NewLB(svc *corev1.Service) (*corev1.Pod, *corev1.ConfigMap) {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      lbPodName(svc),
+			Namespace: defaultNamespace,
+		},
+		Spec: corev1.PodSpec{
+			ServiceAccountName: "testing-tailscale-pod",
+			Containers: []corev1.Container{
+				{
+					Name:  "tailscale",
+					Image: "tailscale/tailscale:stable",
+					Env: []corev1.EnvVar{{
+						Name: "TS_AUTHKEY",
+						ValueFrom: &corev1.EnvVarSource{
+							SecretKeyRef: &corev1.SecretKeySelector{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: defaultSecret,
+								},
+								Key: defaultSecretKey,
+							},
+						},
+					}},
+				},
+				{
+					Name:  "haproxy",
+					Image: "haproxy:2.7",
+					VolumeMounts: []corev1.VolumeMount{{
+						Name:      "haproxy-config",
+						MountPath: "/usr/local/etc/haproxy",
+						ReadOnly:  true,
+					}},
+				},
+			},
+			Volumes: []corev1.Volume{{
+				Name: "haproxy-config",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: lbConfigMapName(svc),
+						},
+					},
+				},
+			}},
+		},
+	}
+
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      lbConfigMapName(svc),
+			Namespace: defaultNamespace,
+		},
+		Data: map[string]string{
+			"haproxy.cfg": renderHaproxyConfig(svc),
+		},
+	}
+
+	return pod, configMap
+}
+
+func renderHaproxyConfig(svc *corev1.Service) string {
+	haproxy_template := `
+# Global parameters
+global
+	maxconn 32000
+
+	# Raise the ulimit for the maximum allowed number of open socket
+	# descriptors per process. This is usually at least twice the
+	# number of allowed connections (maxconn * 2 + nb_servers + 1) .
+	ulimit-n 65535
+
+	uid 0
+	gid 0
+
+	# daemon
+	nosplice
+
+# Default parameters
+defaults
+	# Default timeouts
+	timeout connect 5000ms
+	timeout client 50000ms
+	timeout server 50000ms
+
+{{ $url := printf "%s.%s.svc.cluster.local" .Name .Namespace }}
+
+{{ range .Spec.Ports }}
+frontend localhost-{{ .Port }}
+	bind *:{{ .Port }}
+	option tcplog
+	mode tcp
+	default_backend downstream-{{ .Port }}
+
+backend downstream-{{ .Port }}
+	mode tcp
+	balance roundrobin
+	server downstream {{ $url }}:{{ .Port }} check
+{{ end }}
+`
+
+	tmpl, err := template.New("haproxy_template").Parse(haproxy_template)
+	errcheck.Check(err)
+
+	var buff bytes.Buffer
+	tmpl.Execute(&buff, svc)
+
+	return buff.String()
+}
---
+++ golang-lb-provider/controllers/service_controller.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pthomison/errcheck"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+var (
+	defaultNamespace = "tailscale"
+	defaultSecret    = "tailscale-token"
+	defaultSecretKey = "token"
+)
+
+// ServiceReconciler reconciles a Service object
+type ServiceReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=operator.pthomison.com,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=operator.pthomison.com,resources=services/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=operator.pthomison.com,resources=services/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// Request the service
+	var svc corev1.Service
+	err := r.Get(ctx, req.NamespacedName, &svc)
+	if client.IgnoreNotFound(err) != nil {
+		// requeue in hopes that the error is transient
+		return ctrl.Result{}, err
+	} else if err != nil {
+		// if object has been deleted, ignore
+		return ctrl.Result{}, nil
+	}
+
+	if svc.Spec.Type != "LoadBalancer" {
+		// non LB service, ignore
+		return ctrl.Result{}, nil
+	}
+
+	fmt.Printf("Service: %v/%v/%v\n", svc.Spec.Type, svc.Name, svc.Namespace)
+
+	var pod corev1.Pod
+	err = r.Get(ctx, types.NamespacedName{
+		Name:      lbPodName(&svc),
+		Namespace: defaultNamespace,
+	}, &pod)
+	if client.IgnoreNotFound(err) != nil {
+		// requeue in hopes that the error is transient
+		return ctrl.Result{}, err
+	} else if err == nil {
+		// pod already deployed, ignore
+		return ctrl.Result{}, nil
+	}
+
+	lbPod, cm := NewLB(&svc)
+
+	err = r.Create(ctx, cm)
+	errcheck.Check(err)
+
+	err = r.Create(ctx, lbPod)
+	errcheck.Check(err)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Service{}).
+		Complete(r)
+}
